520a8513fb649ed98613a3602a1674c166e8dd21
Anders Holstborg <jarmammail@gmail.com>
2022-10-26 00:22:09 +0200

    :white_check_mark: try expand_path instead to help circleci

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,7 @@ module IlDaneseSavaggio
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.autoload_paths << "#{config.root}/lib"
-    config.eager_load_paths << "#{config.root}/lib"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
