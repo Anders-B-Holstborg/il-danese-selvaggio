53c26b75cb7f4c55d094212e88a07ca615d06bdb
Anders Holstborg <jarmammail@gmail.com>
2022-10-31 09:48:46 +0100

    :art: add root homepage scaffold

---
+++ spec/requests/homepages_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Homepages', type: :request do
+  describe 'GET /' do
+    # it 'should display the root page' do
+
+    # end
+  end
+end
