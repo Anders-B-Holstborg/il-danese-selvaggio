a383b3bf301bd3d1429e69a6198804be69350ba0
Anders Holstborg <jarmammail@gmail.com>
2022-11-03 21:47:43 +0100

    again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    if config.files_to_run.any? { |path| path.start_with?(Rails.root.join('spec/features').to_s) }
+      Rails.application.load_tasks
+      Rake::Task['tailwindcss:build'].invoke
+    end
+  end
 end
 
 Shoulda::Matchers.configure do |config|
