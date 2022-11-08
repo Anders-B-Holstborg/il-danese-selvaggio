2f7991b0dab307393b2eb546abb4b4f2373c8b25
Anders Holstborg <jarmammail@gmail.com>
2022-11-08 22:48:23 +0100

    :card_file_box: add article model && :heavy_plus_sign: :card_file_box: add and config action_text for article content && :recycle: spec explicit subjects and migrations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
+  subject(:user) { described_class.new({ first_name:, last_name:, nick_name:, email: }) }
+
   let(:first_name) { 'Amy' }
   let(:last_name) { 'Lee' }
   let(:nick_name) { 'Lilith' }
   let(:email) { 'my@immortal.it' }
 
-  subject { described_class.new({ first_name:, last_name:, nick_name:, email: }) }
-
   describe 'fields' do
     describe 'basic attributes' do
-      it 'should have first name, last name, nick_name and email fields' do
-        expect(subject.first_name).to eql(first_name)
-        expect(subject.last_name).to eql(last_name)
-        expect(subject.nick_name).to eql(nick_name)
-        expect(subject.email).to eql(email)
+      it 'responds correctly when invoking first name, last name, nick name and email fields' do
+        expect(user.first_name).to eql(first_name)
+        expect(user.last_name).to eql(last_name)
+        expect(user.nick_name).to eql(nick_name)
+        expect(user.email).to eql(email)
       end
     end
 
@@ -28,13 +28,8 @@ RSpec.describe User, type: :model do
     end
 
     describe '#email' do
-      it { is_expected.to_not allow_value('blah').for(:email) }
+      it { is_expected.not_to allow_value('blah').for(:email) }
       it { is_expected.to allow_value('a@b.com').for(:email) }
     end
-  end
-
-  describe 'relations' do
-    # it { is_expected.to have_many(:articles).dependent(:nullify) }
-    # it { is_expected.to have_many(:comments).dependent(:nullify) }
   end
 end
